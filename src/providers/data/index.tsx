52a1acefc1b2bc50bc0cdbc77e9d57b9ce810dcf
tangxiaochuane@yeah.net <tangxiaochuane@yeah.net>
2024-12-03 11:05:25 +0800

    implement the data provider

--- src/providers/data/index.tsx
+++ src/providers/data/index.tsx
@@ -1,7 +1,11 @@
-import { GraphQLClient } from "@refinedev/nestjs-query";
+import dataProvider, { GraphQLClient } from "@refinedev/nestjs-query";
 import { fetchWrapper } from "./fetch-wrapper";
+import { createClient } from "graphql-ws";
 
 const API_URL = "https://api.crm.refine.dev";
+const API_BASE_URL = "https://api.crm.refine.dev";
+const WS_URL = `wss://api.crm.refine.dev/graphyql`;
+
 export const client = new GraphQLClient(API_URL, {
   fetch: (url: string, options: RequestInit) => {
     try {
@@ -11,3 +15,21 @@ export const client = new GraphQLClient(API_URL, {
     }
   },
 });
+
+export const wsClient =
+  typeof window !== "undefined"
+    ? createClient({
+        url: WS_URL,
+        connectionParams: () => {
+          const accessToken = localStorage.getItem("access_token");
+
+          return {
+            headers: {
+              Authorization: `Bearer ${accessToken}`,
+            },
+          };
+        },
+      })
+    : undefined;
+
+export const graphyqlDataProvider = dataProvider(client);
